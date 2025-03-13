12dffc73582acd951d6aae890304e170f6030069
Bgoodwin24 <bgoodwin24@gmail.com>
2025-03-13 16:45:49 -0700

    feat: Implement RabbitMQ Connection
    - Add conString constant and establish projects connection to RabbitMQ and listen for signal interrupt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const conString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(conString)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v\n", err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Peril is shutting down.")
 }
