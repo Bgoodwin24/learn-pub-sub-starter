3287189b5e83e87737d1a2fadf1e251151755fd2
Bgoodwin24 <bgoodwin24@gmail.com>
2025-03-16 17:44:16 -0700

    feat(pubsub): Limit Prefetch count to 10 with Qos
    - Call `Qos` before `Consume` to set prefetch count to 10
    - Improves performance by preventing a single client from monopolizing messages and speeds up consumption to ~100 messages/second

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -55,6 +55,11 @@ func subscribe[T any](
 		return fmt.Errorf("could not declare and bind queue: %v", err)
 	}
 
+	err = ch.Qos(10, 0, false)
+	if err != nil {
+		return fmt.Errorf("could not set up Quality of Service (Qos) prefetch settings: %w", err)
+	}
+
 	msgs, err := ch.Consume(
 		queue.Name,
 		"",
